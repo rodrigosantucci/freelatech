7acc8d343e99e47aeec62656deae3040d018e7cd
rodrigosantucci <rodrigo_santucci@hotmail.com>
2022-07-17 18:57:44 -0300

    projeto base spring initialzr

---
+++ src/test/java/com/br/freelatech/FreelatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.freelatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
